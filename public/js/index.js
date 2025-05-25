84e83e81d8af09adc58fb4097780222f1be2b079
Tolkinxon <krosstony639@gmail.com>
2025-05-25 19:17:32 +0500

    Finish edit side

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ if(!token) window.location = '/login';
 const socket = io('/app', { auth: {token} });
 
 const elForm = document.querySelector('.js-form');
+const elEditForm = document.querySelector('.js-edit-form');
 const elList = document.querySelector('.carList');
 const elItem = document.querySelector('.carItem').content;
 
@@ -39,7 +40,45 @@ elForm.addEventListener('submit', (evt) => {
     reader.readAsDataURL(file);
 });
 
+elEditForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
 
+    let formData = new FormData(elEditForm);
+    formData = Object.fromEntries(formData)
+
+    const file = formData.image;
+    delete formData.image;
+
+    const reader = new FileReader();
+    const id = window.localStorage.getItem('editId');
+    
+    reader.onload = function () {
+      const base64 = reader.result.split(',')[1]; 
+      socket.emit('editData', { file: base64, formData, id });
+      console.log({ file: base64, formData, id });
+      
+    };
+    reader.readAsDataURL(file);
+    closeEditModal();
+});
+
+elList.addEventListener('click',(evt)=>{
+  const edit = evt.target.matches('.edit');
+  const deletE = evt.target.matches('.delete');
+
+  if(edit){
+    window.localStorage.setItem('editId', evt.target.dataset.id)
+    socket.emit('data');
+    socket.on('getData', (cars)=>{
+      const foundCar = cars.find(item => item.id == evt.target.dataset.id);
+      openEditModal(foundCar.name, foundCar.description, foundCar.price);
+    });
+   }
+  if(deletE){openDeleteModal(); window.localStorage.setItem('deleteId', evt.target.dataset.id)}
+  
+  })
+
+socket.emit('data');
 socket.on('getData', (cars)=>{
   render(cars, elList)
 });
@@ -57,3 +96,46 @@ socket.on('getData', (cars)=>{
   socket.on('uploadError', (data) => {
     alert('Upload failed: ' + data.error);
   });
+
+  socket.on('editSuccess', (data) => {
+    alert(data.message);
+    window.location.reload();
+  });
+
+  socket.on('editError', (data) => {
+    alert('Upload failed: ' + data.error);
+  });
+
+        function openEditModal(name = '', description = '', price = '') {
+            document.getElementById('editModal').style.display = 'block';
+            // document.getElementById('editName').value = name;
+            // document.getElementById('editDescription').value = description;
+            // document.getElementById('editPrice').value = price;
+
+            elEditForm.elements['name'].value = name;
+            elEditForm.elements['price'].value = price;
+            elEditForm.elements['description'].value = description;
+        }
+
+        function closeEditModal() {
+            document.getElementById('editModal').style.display = 'none';
+        }
+
+        function openDeleteModal() {
+            document.getElementById('deleteModal').style.display = 'block';
+        }
+
+        function closeDeleteModal() {
+            document.getElementById('deleteModal').style.display = 'none';
+        }
+
+        function confirmDelete() {
+            closeDeleteModal();
+            alert('Mashina o\'chirildi!');
+        }
+
+        function closeModalOnOutside(event, modalId) {
+            if (event.target.id === modalId) {
+                document.getElementById(modalId).style.display = 'none';
+            }
+        }
--- src/app/appSocketCallBack.js
+++ src/app/appSocketCallBack.js
@@ -9,27 +9,60 @@ appSocketCallBack = async function(socket){
     const checking = await checkToken(token);
     if(!checking.status) return socket.emit('tokenError', {message: checking.message});
     const cars = await readFile('cars');
-    socket.emit('getData', cars);
+    socket.on('data', async ()=>{
+      socket.emit('getData', cars);
+    })
 
-  socket.on('uploadData', async (data) => {
-    try {
-      const buffer = Buffer.from(data.file, 'base64');
-      await writeTempFile('tempFile.jpg', buffer);
-      
-      const result = await cloudinary.uploader.upload(tempFilePath('tempFile.jpg'), {
-        folder: 'cars/assets'
-      });
-     fs.unlink(tempFilePath('tempFile.jpg'));
+    socket.on('uploadData', async (data) => {
+      try {
+        const buffer = Buffer.from(data.file, 'base64');
+        await writeTempFile('tempFile.jpg', buffer);
+        
+        const result = await cloudinary.uploader.upload(tempFilePath('tempFile.jpg'), {
+          folder: 'cars/assets'
+        });
+      await fs.unlink(tempFilePath('tempFile.jpg'));
 
-    const cars = await readFile('cars');
-    const newCar = {id: cars.length?cars.at(-1).id+1:1, ...data.formData, img_url: result.secure_url}
-    cars.push(newCar)
-    await writeFile('cars', cars);
-      socket.emit('uploadSuccess', { message: 'Data successfully uploaded' });
-    } catch (err) {
-      socket.emit('uploadError', { error: err.message });
-    }
-  });
+      const cars = await readFile('cars');
+      const newCar = {id: cars.length?cars.at(-1).id+1:1, ...data.formData, img_url: result.secure_url, public_id: result.public_id}
+      cars.push(newCar)
+      await writeFile('cars', cars);
+        socket.emit('uploadSuccess', { message: 'Data successfully uploaded' });
+      } catch (err) {
+        socket.emit('uploadError', { error: err.message });
+      }
+    });
+    socket.on('editData', async (data) => {
+      try {
+        const cars = await readFile('cars');   
+        const foundIndex = cars.findIndex(item => item.id == data.id);
+
+      if(data.file) {
+          cloudinary.uploader.destroy(cars[foundIndex].public_id, (error, result) => {
+          if (error) {
+            console.error("O'chirish xatosi:", error);
+          } else {
+            console.log("O'chirish muvaffaqiyatli:", result);
+          }
+        }); 
+        
+        const buffer = Buffer.from(data.file, 'base64');
+        await writeTempFile('tempFile.jpg', buffer);
+        const result = await cloudinary.uploader.upload(tempFilePath('tempFile.jpg'), {
+          folder: 'cars/assets'
+        });
+        fs.unlink(tempFilePath('tempFile.jpg'));
+        cars[foundIndex].img_url = result.secure_url;
+      } 
+     
+        cars[foundIndex] = {...cars[foundIndex],...data.formData};
+        await writeFile('cars', cars);
+        socket.emit('editSuccess', { message: 'Data successfully updated' });
+
+      } catch (err) {
+        socket.emit('editError', { error: err.message });
+      }
+    });
 }
 
 module.exports = appSocketCallBack;
